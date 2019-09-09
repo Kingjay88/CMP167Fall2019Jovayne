bead5b10b14caaf13f993cb1156692a2a5774857
Kingjay88 <54905669+Kingjay88@users.noreply.github.com>
2019-09-09 12:40:27 -0400

    Add files via upload

---
+++ sumoftwonumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sumoftwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input= new Scanner (System.in);
+System.out.println("Enter two int values");
+int num1 = input.nextInt();
+int num2 = input.nextInt();
+int result = num1+num2;
+
+System.out.print(result);
+	}
+
+}
+   
---
+++ threenumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class threenumber {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+System.out.println("Enter three int values");
+int num1 = input.nextInt();
+int num2 = input.nextInt();
+int num3 = input.nextInt();
+
+int result = num1+num2+num3;
+System.out.print(result);
+
+	}
+
+}
