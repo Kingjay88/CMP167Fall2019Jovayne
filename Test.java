554071a36d9d3ad955934ee4e54f15f8d22dfe49
Kingjay88 <54905669+Kingjay88@users.noreply.github.com>
2019-09-11 12:37:18 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,30 @@
+package week3;
+/**
+ * 
+ * @author Jovayne Barnes
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.println(78+98); // 176
+//System.out.println('a'+'b'); // 198
+//System.out.println('a'-'b'); // -1
+//System.out.println('0'+'0'); // 96
+//System.out.println('a'+1); // 98
+//System.out.println('a'); // a
+
+int num = 10;
+double dollars = 15.60; 
+String name1 = "Jeff";
+String name2 = "JAY";
+int cash = 888;
+
+System.out.printf("%s gave %s %d dollars ", name1, name2, cash);
+
+
+
+	}
+
+}
