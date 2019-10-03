b8f2ed77bc8ca260706cd1242062656536bb8f1f
Kingjay88 <54905669+Kingjay88@users.noreply.github.com>
2019-10-02 21:13:00 -0400

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Numbers {
+public class Loops {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -10,17 +10,27 @@ public class Numbers {
 				int num2 = input.nextInt();
 				int result = num1 + num2;
 		System.out.println(result);
+	
 		
 		
 		System.out.println("Enter N");
 		int N = input.nextInt();
-	
 		
 		for (int i = 1; i <= N; i++) {
 			System.out.println(i);
 			
+			}
+		System.out.println("The square of ");
+		for (int i1 = 1; i1 <= N; i1++) {
+			System.out.println(i1*i1);
+			
+		}
+		
+		for (int i3 = 1; i3 <= 100; i3++) {
+			if (i3% 2 == 1) continue;
+			System.out.println(i3++);
 		}
 	}
-
+		
 }
-
+	
