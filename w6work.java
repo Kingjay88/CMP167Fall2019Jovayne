03cd71b504653c9b0428f1ee3837ba7cf17b8c6c
Kingjay88 <54905669+Kingjay88@users.noreply.github.com>
2019-10-07 12:40:22 -0400

    Add files via upload

---
+++ w6work.java
@@ -0,0 +1,25 @@
+package w6;
+
+public class w6work {
+
+	public static void main(String[] args) {
+		int result = productThree(123, 34, 69 );
+System.out.println(result);
+	}
+	
+	public static int productThree(int a, int b, int c) {
+		return a * b * c;
+		}
+	
+int result = sumTwo(33, 44);
+
+public static int sumTwo(int j, int d) {
+	int sumOf = 0;
+	for (int i = j; i <= d; i++);
+	sumOf = d + j;
+	return j + d;
+}
+
+}
+
+
