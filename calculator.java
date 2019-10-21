eaa536535d43b7686792ca2ea45a2eb09936c0eb
Kingjay88 <54905669+Kingjay88@users.noreply.github.com>
2019-10-21 12:36:38 -0400

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class calculator {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String SorSF = "";
+		String Operation = "";
+		
+		int numberstouse;
+		
+		System.out.println("Enter the calculator mode")
+		if (SorSF.equals("Standard")) {
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		else if (SorSF.equals("Scientific")) {
+			
+		}
+	}
+
+}
