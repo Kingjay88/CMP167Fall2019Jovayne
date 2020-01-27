bd671bbd7e0b3733bc3bf6c57f2281389154da1b
Kingjay88 <54905669+Kingjay88@users.noreply.github.com>
2020-01-27 10:13:45 -0500

    Add files via upload

---
+++ work1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class work1 {
+
+	public static void main(String[] args) { 
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter name");
+		
+		String name = input.nextLine();
+		System.out.print("Your name is dumb, " +name);
+		System.out.print("!");
+		
+	}
+
+}
